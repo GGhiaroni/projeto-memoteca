dcda6bf0a4667683c9232ec9c49fa8e8b024e2ea
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-08 19:34:19 -0300

    adicionando funcionalidade de mensagem a ser exibida em caso de lista vazia

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import ui from "./ui.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.renderizarDados();
+    verificarMuralVazio();
 
     const formulario = document.getElementById("pensamento-form");
 
@@ -33,4 +34,51 @@ async function handleSubmitFormulario(e)
         alert("Ocorreu um erro ao salvar!");
         throw error;
     }
+}
+
+async function verificarMuralVazio()
+{
+    const feed = await api.buscarDados();
+
+    if (!feed || feed.length === 0) {
+        const main = document.querySelector(".background-overlay");
+
+        const sectionFormulario = document.getElementById("form-container");
+        sectionFormulario.style.display = "none"
+
+        const botaoParaAdicionarUmPensamentoAoFeed = document.createElement("button");
+        botaoParaAdicionarUmPensamentoAoFeed.id = "botao-salvar";
+        botaoParaAdicionarUmPensamentoAoFeed.innerText = "Adicionar pensamentos";
+        botaoParaAdicionarUmPensamentoAoFeed.style.marginTop = "2rem";
+
+        const h2 = main.querySelector("h2");
+
+        h2.insertAdjacentElement("afterend", botaoParaAdicionarUmPensamentoAoFeed);
+
+        botaoParaAdicionarUmPensamentoAoFeed.onclick = () => {
+            sectionFormulario.style.display = "block";
+
+            botaoParaAdicionarUmPensamentoAoFeed.style.display = "none";
+
+            const botaoCancelar = document.getElementById("botao-cancelar");
+            botaoCancelar.onclick = () => {
+                sectionFormulario.style.display = "none";
+                botaoParaAdicionarUmPensamentoAoFeed.style.display = "block";
+            };
+        };
+
+        const mensagemMuralVazio = document.createElement("p");
+        mensagemMuralVazio.textContent = "Nada por aqui ainda, que tal compartilhar alguma ideia?";
+        mensagemMuralVazio.style.textAlign = "center";
+        mensagemMuralVazio.id = "mensagem-mural-vazio";
+
+        const h3 = main.querySelector("h3");
+        h3.insertAdjacentElement("afterend", mensagemMuralVazio);
+
+        const imagemMuralVazio = document.createElement("img");
+        imagemMuralVazio.src = "assets/imagens/lista-vazia.png";
+        imagemMuralVazio.alt = "imagem lista vazia";
+
+        main.appendChild(imagemMuralVazio);
+    }       
 }
