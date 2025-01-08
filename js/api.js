2c6e172591f081677c70ba2300a2e26a2b8e3189
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-08 16:25:38 -0300

    criacao das funcoes responsaveis por fazer a edicao de um dado

--- js/api.js
+++ js/api.js
@@ -22,7 +22,35 @@ const api = {
             });
             return await response.json();
         } catch (error) {
-            alert('Erro ao buscar os dados!');
+            alert('Erro ao salvar dados!');
+            throw error;
+        }
+    },
+
+    async buscarDadoUnicoPorId(id)
+    {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+            return await response.json();
+        } catch (error) {
+            alert("Erro ao buscar dado!");
+            throw error;
+        }
+    },
+
+    async editarDado(dado)
+    {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${dado.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(dado)
+            });
+            return await response.json();
+        } catch (error) {
+            alert("Erro ao editar o dado!");
             throw error;
         }
     }
